cec50d9d4846cf658492d63625a2f53c6df69645
Anderson Pires <devanderson.pires@gmail.com>
2021-11-25 16:18:34 -0300

    :sparkles: criar migration, seeder users e token controller
    criando table users e token

--- app/Models/User.php
+++ app/Models/User.php
@@ -13,21 +13,9 @@ class User extends Model implements AuthenticatableContract, AuthorizableContrac
 {
     use Authenticatable, Authorizable, HasFactory;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email',
-    ];
+    protected $table = ['users'];
+    protected $fillable = ['email'];
+    public $timestamps = false;
 
-    /**
-     * The attributes excluded from the model's JSON form.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-    ];
+    protected $hidden = ['password'];
 }
